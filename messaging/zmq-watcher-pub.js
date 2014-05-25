81ae49146bbdc1099abddfddddb7fefdae02cffe
nie xin <niexin.study@gmail.com>
2014-05-25 20:48:32 +0200

    zmq used

---
+++ messaging/zmq-watcher-pub.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const
+	fs = require('fs'),
+	zmq = require('zmq'),
+	publisher = zmq.socket('pub'),	// publisher endpoint
+	filename = process.argv[2];
+
+fs.watch(filename, function() {
+	// send message to subscribers
+	publisher.send(JSON.stringify({
+		type: 'changed',
+		file: filename,
+		timestamp: Date.now()
+	}));
+});
+
+publisher.bind('tcp://*:5432', function(err) {
+	console.log('Listening for zmq subscribers...');
+});
